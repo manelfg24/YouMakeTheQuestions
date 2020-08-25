c9135765237a0c76562e2f9c6f9f5402a5610f52
Manel <manelfg24@gmail.com>
2020-08-25 11:03:02 +0200

    añadido listo para sinonimos

--- Akinator/Form1.cs
+++ Akinator/Form1.cs
@@ -23,6 +23,10 @@ namespace Akinator
             InitializeComponent();
             tbEnviar = textBox1;
             tbPreguntaGuess = textBox2;
+
+
+           
+
         }
 
         private void button1_Click(object sender, EventArgs e)
@@ -50,7 +54,8 @@ namespace Akinator
                 {
                     foreach (string palabraNew in palabrasNew) //para cada palabra de la pregunta nueva
                     {
-                        if (palabraBase.Equals(palabraNew)) ++count;
+                        //TODO anadir comparacion sinonimos cuando este arreglado
+                        if (palabraBase.Equals(palabraNew)) ++count; //Si las palabras son iguales o sinonimas, + coincidencia
                     }
                 }
 
@@ -68,6 +73,30 @@ namespace Akinator
 
 
 
+        }
+
+        public bool AreSinonimas(string palabraA, string palabraB)
+        {
+            return GetSinonimos(palabraA).Contains(palabraB);
+        }
+
+        public ArrayList GetSinonimos(string palabra)
+        //Funcion que, dada una palabra, devuelve un ArrayList de strings con los sinonimos
+        //en español de esta palabra.
+        {
+            ArrayList sinonimosArray = new ArrayList();
+            var appWord = new Microsoft.Office.Interop.Word.Application();
+            object objLanguage = Microsoft.Office.Interop.Word.WdLanguageID.wdSpanish;
+            Microsoft.Office.Interop.Word.SynonymInfo si = appWord.get_SynonymInfo(palabra, ref (objLanguage));
+            foreach (var meaning in (si.MeaningList as Array))
+            {
+                sinonimosArray.Add(meaning.ToString());
+            }
+            appWord.Quit(); //include this to ensure the related process (winword.exe) is correctly closed. 
+            System.Runtime.InteropServices.Marshal.ReleaseComObject(appWord);
+            objLanguage = null;
+            appWord = null;
+            return sinonimosArray;
         }
     }
 }
